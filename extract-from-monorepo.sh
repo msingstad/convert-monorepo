a5e5cc2b91543272bc11c235e97e995838eec673
Martin Singstad <40832199+msingstad@users.noreply.github.com>
2023-02-06 14:21:03 +0100

    Update extract-from-monorepo.sh

--- extract-from-monorepo.sh
+++ extract-from-monorepo.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
-cd /Users/martinsingstad/Projects
+PATH="/Users/martinsingstad/Projects"
+cd $PATH
 
 for folder in */biztalk-applications/ ; do
     folderName=${folder%/}
     repoName=${folderName#Norwegian.}
-    echo $repoName
+    
+    #Create new branch that contains the folder only and keeps commit log
     git subtree split -P $folderName -b $repoName
+    
+    #Pull new branch into new git folder
     cd ..
     mkdir $repoName && cd $repoName
     git init
     git checkout -b main
-    git pull "/Users/martinsingstad/Projects/biztalk-applications" $repoName
+    git pull "$PATH/biztalk-applications" $repoName
+    
+    #Make repo ready for push and push to remote
     git remote add origin "git@bitbucket.org:norwegianairshuttle/$repoName.git"
+    git config pull.rebase true
+    git pull origin main
     git push -u origin main
-    cd /Users/martinsingstad/Projects/biztalk-applications
+    
+    #Remove project-folder from monorepo (just to keep track of un-moved projects)
+    cd $PATH/biztalk-applications
     rm -rf $folderName
+    cd ..
 done
 
